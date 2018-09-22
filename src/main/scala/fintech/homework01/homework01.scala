b49351db6f20585588d6aa8fb13bb1617bf14d15
zitt <NKtargeting@yandex.ru>
2018-09-22 21:18:57 +0500

    Split the code into parts for better readability

--- src/main/scala/fintech/homework01/homework01.scala
+++ src/main/scala/fintech/homework01/homework01.scala
@@ -30,10 +30,12 @@ package fintech.homework01
 // и т.д.
 
 class Hangman(io: IODevice) {
+  private var hiddenWord = ""
+  private var currentStage = -1
+  private var continue = true
+
   def play(word: String): Unit = {
-    var hiddenWord = "_" * word.length
-    var continue = true
-    var currentStage = -1
+    hiddenWord = "_" * word.length
 
     do {
       io.printLine("Word: " + hiddenWord)
@@ -43,26 +45,36 @@ class Hangman(io: IODevice) {
       val foundIndices = word.zipWithIndex.filter(_._1 == userLetter).map(_._2)
 
       if (foundIndices.nonEmpty) {
-        foundIndices.foreach(i =>
-          hiddenWord = hiddenWord.updated(i, userLetter)
-        )
-        if (currentStage >= 0)
-          io.printLine(stages(currentStage))
-
-        if (!hiddenWord.contains("_")) {
-          io.printLine("Word: " + hiddenWord)
-          io.printLine("You win!")
-          continue = false
-        }
+        userRight(foundIndices, userLetter)
       } else {
-        currentStage += 1
-        io.printLine(stages(currentStage))
-        if (currentStage == stages.length - 1) {
-          io.printLine("You are dead")
-          continue = false
-        }
+        userWrong()
       }
     } while (continue)
+  }
+
+  def userRight(foundIndices: IndexedSeq[Int], userLetter: Char): Unit = {
+    foundIndices.foreach(i =>
+      hiddenWord = hiddenWord.updated(i, userLetter)
+    )
+
+    if (currentStage >= 0)
+      io.printLine(stages(currentStage))
+
+    if (!hiddenWord.contains("_")) {
+      io.printLine("Word: " + hiddenWord)
+      io.printLine("You win!")
+      continue = false
+    }
+  }
+
+  def userWrong(): Unit = {
+    currentStage += 1
+    io.printLine(stages(currentStage))
+
+    if (currentStage == stages.length - 1) {
+      io.printLine("You are dead")
+      continue = false
+    }
   }
 
   val stages = List(
